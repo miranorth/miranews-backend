5ec2eddb6052766049aec8b301d3fa018f96bfa3
fabiokounang <fabio_kosukeueki@yahoo.com>
2018-02-08 20:22:45 +0700

    model user

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+const express    = require('express');
+const path       = require('path');
+const logger     = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose   = require('mongoose')
+const user       = require('./routes/user');
+const cors       = require('cors');
+const app        = express();
 
-const index = require('./routes/index');
+mongoose.connect("mongodb://fabio:fabio@ds229648.mlab.com:29648/miranorth")
 
-const app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', index);
+app.use('/users', user);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.send('error');
 });
