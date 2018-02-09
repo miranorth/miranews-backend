83facf5d972b2a3c169a867f28a76ca300afc1ec
fabiokounang <fabio_kosukeueki@yahoo.com>
2018-02-09 14:54:46 +0700

    verify nexmo dan register database

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,8 +39,8 @@ class UserController {
 
     objUser.save().then((result) => {
       res.status(200).json({
-        msg  : 'Sign Up Success !',
-        data : result
+        msg: 'Success Sign Up',
+        data: result
       })
     }).catch((err) => {
       res.status(500).json({
