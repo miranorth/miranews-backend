bc62a308cb58e8904a39292c4c1b829e1119d319
yulianhp <kono.utsukushi24@gmail.com>
2018-02-09 11:59:10 +0700

    nexmo backend done

---
+++ controllers/nexmo.js
@@ -0,0 +1,47 @@
+var Nexmo = require('nexmo');
+
+var nexmo = new Nexmo({
+    apiKey: process.env.NEXMO_KEY,
+    apiSecret: process.env.NEXMO_SECRET
+  });
+  
+class Verification {
+  static reqVer(req,res){
+    nexmo.verify.request(
+      {number:req.body.number,brand:'Miranews'},(err, response) => {
+        if (err) {
+          res.status(400)
+          .json({
+            error:err
+          })
+        } else {
+          res.status(200)
+          .json({
+            msg: 'Verification request sent',
+            data: response
+          })
+        }
+      });
+  }
+  
+  static verCheck(req,res){
+    nexmo.verify.check(
+      {request_id:req.body.request_id,code:req.body.code},(err, response) => {
+        if (err) {
+          res.status(400)
+          .json({
+            error:err
+          })
+        } else {
+          res.status(200)
+          .json({
+            msg: 'Validated',
+            data: response
+          })
+        }
+      });
+
+  }
+}
+
+module.exports = Verification;
